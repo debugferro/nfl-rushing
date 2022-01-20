4ccc8c1049fe25f5f6ca7bad0ee73f0e8173c3a0
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-01-19 22:08:10 -0300

    Created PlayerView for rushings.json.

---
+++ server/lib/nfl_rushing_web/views/player_view.ex
@@ -0,0 +1,28 @@
+defmodule NflRushingWeb.Api.V1.PlayerView do
+  use NflRushingWeb, :view
+
+  def render("rushings.json", %{rushings: rushings}) do
+    %{rushings: render_many(rushings, NflRushingWeb.Api.V1.PlayerView, "rushing.json", as: :rushing)}
+  end
+
+  def render("rushing.json", %{rushing: rushing}) do
+    %{
+      "player" => rushing.player,
+      "team" => rushing.team,
+      "pos" => rushing.pos,
+      "attempts_avg" => rushing.attempts_avg,
+      "attempts" => rushing.attempts,
+      "total_yards" => rushing.total_yards,
+      "yards_per_att" => rushing.yards_per_att,
+      "yards_per_game" => rushing.yards_per_game,
+      "total_touchdowns" => rushing.total_touchdowns,
+      "longest_rush" => rushing.longest_rush,
+      "longest_rush_td" => rushing.longest_rush_td,
+      "first_downs" => rushing.first_downs,
+      "first_downs_pct" => rushing.first_downs_pct,
+      "twenty_yards_plus" => rushing.twenty_yards_plus,
+      "forty_yards_plus" => rushing.forty_yards_plus,
+      "fumbles" => rushing.fumbles
+    }
+  end
+end
