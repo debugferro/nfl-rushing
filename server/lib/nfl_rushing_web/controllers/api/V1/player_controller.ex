15d2a0b4e0b56c0bea7f4c57a65c4f3787c5d1cf
Gabriel Ferro <gabrielferroroque@hotmail.com>
2022-01-21 15:52:46 -0300

    Refactored PlayerController by creating private methods making it concise.

--- server/lib/nfl_rushing_web/controllers/api/V1/player_controller.ex
+++ server/lib/nfl_rushing_web/controllers/api/V1/player_controller.ex
@@ -1,20 +1,19 @@
 defmodule NflRushingWeb.Api.V1.PlayerController do
-  alias NflRushing.{Player, Player.Rushing, Repo}
+  alias NflRushing.{Player, Player.CSVBuilder}
   use NflRushingWeb, :controller
 
-  def index(conn, %{"sort_by" => sort_arg}) do
-    IO.inspect(sort_arg)
-    rushings = Player.get_rushing_ordered_by(sort_arg)
-    render(conn, "rushings.json", rushings: rushings)
-  end
-
-  def index(conn, %{"q" => searched_name}) do
-    rushings = Player.search_by_name(searched_name)
-    render(conn, "rushings.json", rushings: rushings)
-  end
-
   def index(conn, params) do
-    IO.inspect(params)
-    render(conn, "rushings.json", rushings: Repo.all(Rushing))
+    rushings = request_data(params)
+    render(conn, "rushings.json", rushings: rushings)
   end
+
+  def download_csv(conn, params) do
+    csv_file = request_data(params) |> CSVBuilder.write()
+    send_download(conn, {:binary, csv_file}, content_type: "text/csv", filename: "rushing-statistics.csv")
+  end
+
+  defp request_data(%{"sort_by" => sort_args, "q" => search_term}), do: Player.search_by_name(search_term, sort_args)
+  defp request_data(%{"sort_by" => sort_args}), do: Player.get_rushing_ordered_by(sort_args)
+  defp request_data(%{"q" => search_term}), do: Player.search_by_name(search_term)
+  defp request_data(_params), do: Player.get_all()
 end
